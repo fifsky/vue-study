64d18a1aee392e498fb49cf129c9db94718e6667
fifsky <fifsky@gmail.com>
2018-12-20 18:57:33 +0800

    add header

--- api/handler/user.go
+++ api/handler/user.go
@@ -11,22 +11,3 @@ var AdminUser core.HandlerFunc = func(c *core.Context) core.Response {
 	user := getLoginUser(c)
 	return c.Success(user)
 }
-
-
-var UserList core.HandlerFunc = func(c *core.Context) core.Response {
-	p := struct {
-		Id int `json:"id"`
-	}{}
-
-	if err := c.ShouldBindJSON(p); err != nil {
-		return c.Fail(201, err)
-	}
-
-	users := make([]*model.Users,0)
-	err := gosql.Model(&users).Limit(10).All()
-	if err != nil {
-		c.Fail(500, err)
-	}
-
-	return c.Success(users)
-}
