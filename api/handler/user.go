bc5f2ba888c97a07e76c3d0f862648d9ee407ca7
fifsky <fifsky@gmail.com>
2018-12-21 09:50:42 +0800

    add header

--- api/handler/user.go
+++ api/handler/user.go
@@ -1,11 +1,8 @@
 package handler
 
 import (
-	"app/model"
-	"github.com/ilibs/gosql"
 	"github.com/ilibs/very/core"
 )
-
 
 var AdminUser core.HandlerFunc = func(c *core.Context) core.Response {
 	user := getLoginUser(c)
